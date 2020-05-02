7c5d11426cac8a799659f06f46193b16c8dd5664
AtulRaj151 <37062660+AtulRaj151@users.noreply.github.com>
2020-05-02 12:00:07 +0530

    add js functionalities

--- script.js
+++ script.js
@@ -4,30 +4,55 @@ var operand1  =  0;
 var operand2 = null;
 var operator = null;
 
+function isOperator(value) {
+
+    return value == "+" || value =="-" || value =="*" || value =="/";
+}
+
 for(var i = 0; i< button.length; i++){
       button[i].addEventListener('click',function(){
           var value = this.getAttribute('data-value');
-          
-          if(value == '+'){
-            operator = '+';
-            operand1 =  parseFloat(display.textContent);
-            display.innerText = "";
+          var text  = display.textContent.trim();
 
-          } else if(value == '-'){
+          if(isOperator(value)){
 
+              operator = value;
+              operand1  = parseFloat(text);
+              display.textContent = "";
+          }else if( value =="ac") {
+                 display.textContent = "";
+          } else if(value == "sign"){
 
-          }else if( value == '='){
-              operand2 = parseFloat(display.textContent);
-              display.innerText = operand2;
+              operand1  = parseFloat(text);
+              operand1 = -1* operand1;
+              display.textContent = operand1;
+          } else if (value == ".") {
+               if(text.length && !text.includes(".")) {
 
-          }else if(value== "clear"){
+                 display.textContent = text + "."
+               }
+          }else if( value == "%"){
+              operand1 = parseFloat(text);
+              operand1 = operand1 / 100;
+              display.textContent = operand1;
+          }else if( value == "="){
 
-               display.innerText = "";
+             operand2 = parseFloat(text);
+             var result = eval(operand1 +' '+operator +' ' + operand2);
+             display.textContent = result;
+              if(result) {
+
+                 display.textContent = result;
+                 operand1 = result;
+                 operand2 = null;
+                 operator = null;
+              }
+
+          }else{
+
+              display.textContent += value;
           }
           
-          else{
-              display.innerText += value;
-          }
         
       });
 }
